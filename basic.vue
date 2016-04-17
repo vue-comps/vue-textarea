d946b8fa2166d823161ec2df02c0d4beaa950dcb
Paul Pflugradt <paulpflugradt@googlemail.com>
2016-04-17 13:03:06 +0200

    Updates

---
+++ basic.vue
@@ -0,0 +1,25 @@
+<template lang="jade">
+.container
+  a(href="https://github.com/vue-comps/vue-textarea/blob/master/dev/basic.vue") source
+  vc-textarea(:size="{height:50}",:max-size="{height:200,width:200}")
+    span(slot="placeholder") a placeholder
+  p pre-filled with `max-width=200`:
+  vc-textarea(
+    :size="{height:50}",
+    :max-size="{height:200,width:200}",
+    ) Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+    span(slot="placeholder") a placeholder
+  p pre-filled without `max-width`
+  vc-textarea(:size="{height:50}",:max-size="{height:200}") Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+    span(slot="placeholder") a placeholder
+  p pre-filled with hover
+  vc-textarea(:size="{height:50}",:max-size="{height:200,width:200}",:hover-size="{height:150,width:180}") Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+    span(slot="placeholder") a placeholder
+</template>
+
+<script lang="coffee">
+module.exports =
+  components:
+    "vc-textarea" : require "../src/textarea.vue"
+
+</script>
